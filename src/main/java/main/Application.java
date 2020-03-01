79ff0462aee830db3df37abb25dd75b87f2b7f36
Verbőczy Kristóf <verbocika@gmail.com>
2020-03-01 18:36:53 +0100

    Use interface instead of concrete class

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,9 +1,9 @@
 package main;
 
-import dto.DeletePagesArguments;
 import dto.ExtractPagesArguments;
 import javafx.util.Pair;
 import service.BasicPdfService;
+import service.PdfService;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -12,7 +12,7 @@ public class Application {
 
     public static void main(final String[] args) {
         System.out.println("Hello world!");
-        BasicPdfService basicPdfService = new BasicPdfService();
+        PdfService basicPdfService = new BasicPdfService();
         try {
             //basicPdfService.removePages(new DeletePagesArguments("C:\\Users\\verboczy\\Desktop\\ssh.pdf", Arrays.asList(0, 1)));
             basicPdfService.extractPages(new ExtractPagesArguments("C:\\Users\\verboczy\\Desktop\\pdfattachment\\viking.pdf", Arrays.asList(new Pair<>(3, 14), new Pair<>(0, 143)), Arrays.asList("elso.pdf", "masodik2.pdf")));
